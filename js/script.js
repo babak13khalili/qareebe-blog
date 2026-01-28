69088395898c2c0434fd35f27c853f83b1926342
babak <babakkhalili1303@gmail.com>
2026-01-28 14:06:04 +0100

    V0.1.4.3

--- js/script.js
+++ js/script.js
@@ -2,20 +2,18 @@ const langToggle = document.getElementById("lang-toggle");
 const themeToggle = document.getElementById("theme-toggle");
 const htmlTag = document.documentElement;
 
-// --- ۱. مدیریت زبان (ساده و دایره‌ای) ---
 langToggle.addEventListener("click", () => {
   const isRtl = htmlTag.dir === "rtl";
-  const langText = document.getElementById("lang-text");
 
   if (isRtl) {
     htmlTag.dir = "ltr";
     htmlTag.lang = "en";
-    langText.textContent = "FA";
+    langToggle.textContent = "فـا";
     updateText("en");
   } else {
     htmlTag.dir = "rtl";
     htmlTag.lang = "fa";
-    langText.textContent = "EN";
+    langToggle.textContent = "En";
     updateText("fa");
   }
 });
@@ -23,23 +21,27 @@ langToggle.addEventListener("click", () => {
 function updateText(lang) {
   const content = {
     en: {
+      // حتما حروف کوچک
       title: "Thoughts & Memories",
       desc: "To record, review, and legacy",
       pageTitle: "Thoughts, Life & Daily Notes",
     },
     fa: {
+      // حتما حروف کوچک
       title: "افکار، احوال و روزمرگی‌ها",
       desc: "برای ثبت، مرور و میراث",
       pageTitle: "افکار، احوال و روزمرگی‌ها",
     },
   };
 
-  document.getElementById("main-title").textContent = content[lang].title;
-  document.getElementById("main-desc").textContent = content[lang].desc;
-  document.getElementById("page-title").textContent = content[lang].pageTitle;
+  // چک کردن برای جلوگیری از ارور احتمالی
+  if (content[lang]) {
+    document.getElementById("main-title").textContent = content[lang].title;
+    document.getElementById("main-desc").textContent = content[lang].desc;
+    document.getElementById("page-title").textContent = content[lang].pageTitle;
+  }
 }
 
-// --- ۲. مدیریت تم ---
 themeToggle.addEventListener("click", () => {
   const currentTheme = htmlTag.getAttribute("data-theme");
   const newTheme = currentTheme === "dark" ? "light" : "dark";
